b8d8becc7d71883b87b64855e281057ab3a38446
yanavlitv <yana.v.litv@gmail.com>
2025-10-01 17:12:43 +0300

    Лабораторная работа 3. Литвиновская Яна 501994

---
+++ Tree.py
@@ -0,0 +1,37 @@
+def build(pos, left, right, value, tree, current_height, max_hight, root):
+    """
+       Рекурсивно строит бинарное дерево.
+
+       Аргументы:
+           pos: Текущая позиция в массиве
+           left: Левый индекс диапазона
+           right: Правый индекс диапазона
+           value: Текущее значение узла
+           tree: Массив для хранения дерева
+           current_height: Текущая высота
+           max_height: Максимальная высота дерева
+           root: Корневое значение
+    """
+    if right - left == 1:
+        tree[pos] = value
+        return
+    else:
+        tree[pos] = root
+    middle = (right + left) // 2
+    build(2*pos + 1, left, middle, root*3+1, tree, current_height+1, max_hight, root*3+1)
+    build(2*pos + 2, middle + 1, right, root*3-1, tree, current_height+1, max_hight, root*3-1)
+    return
+
+
+def gen_bin_tree(height = 5, root = 10):
+    t = [0] * (2**height - 1)
+    build(0, 0, 2**height - 1, 0,  t, 1, height, root)
+    return t
+
+
+
+
+
+
+
+
